35c69fa0c1bfd3fd00133ed5a2d3eb3f9b75b488
Niko Lazur <74565339+LazurNiko@users.noreply.github.com>
2021-12-29 14:41:45 +0200

    added PO for sidebar

--- cypress/integration/PO/Sidebar.js
+++ cypress/integration/PO/Sidebar.js
@@ -1,7 +1,57 @@
 class sidebar {
+  login() {
+    cy.visit('/signin');
+    cy.get("#username").type('Snow');
+    cy.get("#password").type('12345Qwert!');
+    cy.get('[data-test="signin-submit"]').click();
+  }
   sidebarMenu() {
     return cy.get('.MuiDrawer-root.MuiDrawer-docked');
   }
-
+  avatar() {
+    return cy.get('.MuiSvgIcon-root.MuiAvatar-fallback').should('be.visible');
+  }
+  username() {
+    return cy.get('[data-test="sidenav-user-full-name"]').should('contain', 'John');
+  }
+  userFirstName() {
+    return cy.get('[data-test="sidenav-username"]').should('contain', 'Snow');
+  }
+  accountBalanceAmount() {
+    return cy.get('[data-test="sidenav-user-balance"]').should('contain', '$0.00');
+  }
+  accountBalanceTitle() {
+    return cy.get('h6.MuiTypography-subtitle2').eq(1).should('contain', 'Account Balance');
+  }
+  homeIcon() {
+    return cy.get('.MuiListItemIcon-root').eq(0).find('svg');
+  }
+  myAccountIcon() {
+    return cy.get('.MuiListItemIcon-root').eq(1).find('svg');
+  }
+  bankAccountsIcon() {
+    return cy.get('.MuiListItemIcon-root').eq(2).find('svg');
+  }
+  notificationsIcon() {
+    return cy.get('.MuiListItemIcon-root').eq(3).find('svg');
+  }
+  logoutIcon() {
+    return cy.get('.MuiListItemIcon-root').eq(4).find('svg');
+  }
+  homeLink() {
+    return cy.get('[data-test="sidenav-home"]').should('have.attr', 'href', '/').and('be.visible');
+  }
+  myAccountLink() {
+    return cy.get('[data-test="sidenav-user-settings"]').should('have.attr', 'href', '/user/settings').and('be.visible');
+  }
+  bankAccountsLink() {
+    return cy.get('[data-test="sidenav-bankaccounts"]').should('have.attr', 'href', '/bankaccounts').and('be.visible');
+  }
+  notificationsLink() {
+    return cy.get('[data-test="sidenav-notifications"]').should('have.attr', 'href', '/notifications').and('be.visible');
+  }
+  logoutLink() {
+    return cy.get('[data-test="sidenav-signout"]').should('be.visible').contains('Logout');
+  }
 }
 export default sidebar;
--- cypress/integration/Sidebar.spec.js
+++ cypress/integration/Sidebar.spec.js
@@ -1,45 +1,32 @@
 import sidebar from './PO/Sidebar';
 
 describe('Sidebar', () => {
-  const SideBar = new sidebar();
-  let existingUser = {
-    "username": "Snow",
-    "password": "12345Qwert!"
-  };
+  const sideBar = new sidebar();
 
-  beforeEach(() => {
-    cy.visit('/signin');
-    cy.get("#username").type(existingUser.username);
-    cy.get("#password").type(existingUser.password);
-    cy.get('[data-test="signin-submit"]').click();
-    // cy.login();
+  before(() => {
+    sideBar.login();
   });
 
   it('Sidebar has avatar, username, account balance and menu list', () => {
-    SideBar.sidebarMenu()
+    sideBar.sidebarMenu()
       .should('have.attr', 'data-test', 'sidenav')
       .and('be.visible')
       .within(() => {
-        cy.get('.MuiSvgIcon-root.MuiAvatar-fallback').should('be.visible');
-        cy.get('[data-test="sidenav-user-full-name"]').should('contain', 'John');
-        cy.get('[data-test="sidenav-username"]').should('contain', 'Snow');
-        cy.get('[data-test="sidenav-user-balance"]').should('contain', '$0.00');
-        cy.get('h6.MuiTypography-subtitle2').eq(1).should('contain', 'Account Balance');
-        cy.get('.MuiListItemIcon-root').eq(0).find('svg');
-        cy.get('[data-test="sidenav-home"]').should('have.attr', 'href', '/').and('be.visible');
-        cy.get('.MuiListItemText-root').eq(0).find('span').contains('Home');
-        cy.get('.MuiListItemIcon-root').eq(1).find('svg')
-        cy.get('[data-test="sidenav-user-settings"]').should('have.attr', 'href', '/user/settings').and('be.visible')
-        cy.get('.MuiListItemText-root').eq(1).find('span').contains('My Account')
-        cy.get('.MuiListItemIcon-root').eq(2).find('svg')
-        cy.get('[data-test="sidenav-bankaccounts"]').should('have.attr', 'href', '/bankaccounts').and('be.visible')
-        cy.get('.MuiListItemText-root').eq(2).find('span').contains('Bank Accounts')
-        cy.get('.MuiListItemIcon-root').eq(3).find('svg')
-        cy.get('[data-test="sidenav-notifications"]').should('have.attr', 'href', '/notifications').and('be.visible')
-        cy.get('.MuiListItemText-root').eq(3).find('span').contains('Notifications')
-        cy.get('.MuiListItemIcon-root').eq(4).find('svg')
-        cy.get('[data-test="sidenav-signout"]').should('be.visible')
-        cy.get('.MuiListItemText-root').eq(4).find('span').contains('Logout')
+        sideBar.avatar();
+        sideBar.username();
+        sideBar.userFirstName();
+        sideBar.accountBalanceAmount();
+        sideBar.accountBalanceTitle();
+        sideBar.homeIcon();
+        sideBar.homeLink();
+        sideBar.myAccountIcon();
+        sideBar.myAccountLink();
+        sideBar.bankAccountsIcon();
+        sideBar.bankAccountsLink();
+        sideBar.notificationsIcon();
+        sideBar.notificationsLink();
+        sideBar.logoutIcon();
+        sideBar.logoutLink();
     });
   });
 });
